4c55940f4f1a13dea99816b962942b7cc35511a5
Mighty <98230407+BeingMighty@users.noreply.github.com>
2022-07-19 01:17:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,49 @@
+# MightyXSpam | @MightyXSpam
+
+"Fill These Vars Accordingly"
+"Visit @MightyXSupport If You Need Assistance"
+
+API_ID = int(12345678) 
+
+API_HASH = "abcdefghijklmnopqrstuvwxyz"
+
+ALIVE_NAME = "MightyX" # Optional 
+
+ALIVE_PIC = "https://telegra.ph/file/4c2ed8d25ba0eb913fd2f.jpg" # Optional 
+
+ALIVE_TEXT = "╚»★ 𝙏𝙃𝙀 𝙈𝙄𝙂𝙃𝙏𝙔 𝙓 𝙎𝙋𝘼𝙈 ★«╝" # Optional 
+
+OWNER_ID = int(12345678910)
+
+SUDO_USERS = ("").split() # Ex : "12345678 12345678" # Optional
+
+CMD_HNDLR = "!" # Optional 
+
+BOT_TOKEN = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN2 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN3 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN4 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN5 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN6 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN7 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN8 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN9 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+BOT_TOKEN10 = "5292487862:AAbCdEfgHIjklmnOpqrsTuVwxyZ"
+
+DATABASE_URL = "postgres://abcdefg:abcDeFghiJklmnoPqrsTuvwxyz@castor.db.elephantsql.com/abcdefg"
+
+
+# 𝐃𝐨𝐧'𝐭 𝐓𝐨𝐮𝐜𝐡 𝐓𝐡𝐞 𝐕𝐚𝐫𝐬 𝐁𝐞𝐥𝐨𝐰 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞 !! 
+
+ORIGINAL_CODE = "aHR0cHM6Ly9naXRodWIuY29tL0JlaW5nTWlnaHR5L01pZ2h0eUJvdFNwYW1TSA=="
+VARIFICATION = "TWlnaHR5WCBPUCBCYWFraSBTYWIgTHVuZCBLaSBUb3BpICEh"
+PASS = "VGFrIERpayBLYW5nZXIgISEgS2FuZyBXaXRoIENyZWRpdHMgTWFkYWZha2Eu"
