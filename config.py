ef6ad752fef45e73562ed5c091d433a0711e8c8e
R.ONE <94272759+Nikki0005@users.noreply.github.com>
2022-08-29 00:34:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,41 +6,41 @@ from dotenv import load_dotenv
  
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
+API_ID = int(os.getenv("13670329"))
 
-API_HASH = os.getenv("API_HASH")
+API_HASH = os.getenv("a2999a316b7f4b0b62ea476ab38dab0a")
 
-ALIVE_NAME = os.getenv("ALIVE_NAME")
+ALIVE_NAME = os.getenv("@R_ONE_HU")
 
-ALIVE_PIC = os.getenv("ALIVE_PIC")
+ALIVE_PIC = os.getenv("https://telegra.ph//file/efe50d02d3a9943e60ed4.jpg")
 
-ALIVE_TEXT = os.getenv("ALIVE_TEXT")
+ALIVE_TEXT = os.getenv("innocent hu but ungli mt Krna")
 
-OWNER_ID = int(os.getenv("OWNER_ID"))
+OWNER_ID = int(os.getenv("5769566212"))
 
-SUDO_USERS = os.getenv("SUDO_USERS").split()
+SUDO_USERS = os.getenv("952926647 5265276618").split()
 
-CMD_HNDLR = os.getenv("CMD_HNDLR", "!")
+CMD_HNDLR = os.getenv("CMD_HNDLR", ".")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("5272804087:AAF4VF8uprfJ4DeN7xAyzU-UClYDCZHkR2k")
 
-BOT_TOKEN2 = os.getenv("BOT_TOKEN2")
+BOT_TOKEN2 = os.getenv("5113991932:AAFgtNMT1o4XtU1WF1Urs0zvuZVqp0rplos")
 
-BOT_TOKEN3 = os.getenv("BOT_TOKEN3")
+BOT_TOKEN3 = os.getenv("5279883936:AAGIADEdNP9pb-NdTX-QiQVlEV-Fwus7TnE")
 
-BOT_TOKEN4 = os.getenv("BOT_TOKEN4")
+BOT_TOKEN4 = os.getenv("5185226667:AAEXrQr42kAvJzro2xnVJr95YyPPWx08IDA")
 
-BOT_TOKEN5 = os.getenv("BOT_TOKEN5")
+BOT_TOKEN5 = os.getenv("5275386312:AAGtVXIL7hFTRwn8-VKTyoyArSK7cUTHXRM")
 
-BOT_TOKEN6 = os.getenv("BOT_TOKEN6")
+BOT_TOKEN6 = os.getenv("5342399403:AAGzc7GgPv3s_iFiokaXRs9rqiHxWHNJdf0")
 
-BOT_TOKEN7 = os.getenv("BOT_TOKEN7")
+BOT_TOKEN7 = os.getenv("5318944724:AAH7d8t9qPoy9SOpI_-xvM3zm44uCXVJwCw")
 
-BOT_TOKEN8 = os.getenv("BOT_TOKEN8")
+BOT_TOKEN8 = os.getenv("5225247900:AAF2w0OCCqHjmn1Nsn12I4Iw-UaxdxIJS6s")
 
-BOT_TOKEN9 = os.getenv("BOT_TOKEN9")
+BOT_TOKEN9 = os.getenv("5632120185:AAGGWETVv2TPJBCWiYtrF6VtGxXnZUiHdiE")
 
-BOT_TOKEN10 = os.getenv("BOT_TOKEN10")
+BOT_TOKEN10 = os.getenv("5691557888:AAHpzyOn4TkKPB6EehFd0I-6jys4DLzQvKg")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
